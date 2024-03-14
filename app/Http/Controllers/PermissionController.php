032d92ad4e86f927426d7a07fa5dfc9c7c340c5e
eric yullu <ndeti.yullu@gmail.com>
2024-03-14 13:57:47 +0300

    second commit

---
+++ app/Http/Controllers/PermissionController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PermissionController extends Controller
+{
+    //for creating the persmission folder
+    public function index()
+    {
+        return view('permissions.index');
+    }
+    public function create()
+    {
+
+    }
+    public function store()
+    {
+
+    }
+    public function edit()
+    {
+
+    }
+    public function update()
+    {
+
+    }
+    public function destroy()
+    {
+
+    }
+}
